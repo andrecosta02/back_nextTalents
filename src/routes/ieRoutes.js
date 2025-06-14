3b1eeb7f16fa142c386f24aa1167b68cc13be849
Andre Costa <andrevgcosta02@gmail.com>
2025-06-14 14:26:20 -0300

    Add id in route update IE

--- src/routes/ieRoutes.js
+++ src/routes/ieRoutes.js
@@ -8,7 +8,7 @@ router.post("/login", ieController.login)  // Login - IE
 router.get("/list", ieController.listActive)  // Listar IEs ativos
 router.post("/register", ieController.register)  // Registro - IE
 router.post("/confirm-email", ieController.confirmEmail); // Confirmação de e-mail - IE
-router.put('/update', authMiddleware, ieController.update);  // Update - IE
+router.put('/update/:id', authMiddleware, ieController.update);  // Update - IE
 router.post("/forgot-pass", ieController.forgotPass)  // Esqueci senha - IE
 router.post("/reset-pass", ieController.resetPass)  // Resetar senha - IE
 
